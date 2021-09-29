262751f72f78e8152e7147add7d8d47904c963ef
markrapiz2001 <mrapiz@calpoly.edu>
2021-09-29 11:23:55 -0700

    Finished part 4

--- backend.js
+++ backend.js
@@ -1,3 +1,34 @@
+const users = {
+    users_list:
+        [
+            {
+                id: 'xyz789',
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                id: 'abc123',
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                id: 'ppp222',
+                name: 'Mac',
+                job: 'Professor',
+            },
+            {
+                id: 'yat999',
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                id: 'zap555',
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ]
+}
+
 const express = require('express');
 const app = express();
 const port = 5000;
@@ -11,3 +42,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+const findUserByName = (name) => {
+    return users['users_list'].filter((user) => user['name'] === name);
+}
